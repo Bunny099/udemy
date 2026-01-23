8a9f28ea8fab7babf9b2b01fc7cef57cc4e5ac1b
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-23 22:41:17 +0530

    feat:register route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
 import express, { json } from "express";
 import cors from "cors"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+import { prismaDb } from "./lib/db.js";
+
+
 const app = express();
+
 app.use(cors());
 app.use(json())
-app.get("/hello",(req,res)=>{
-    res.send("hello")
+let AUTH_SECRET = process.env.AUTH_SECRET!;
+
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
+
+        if (!name || !email || !password || !role) {
+            return res.status(400).json({ message: "Field missing!" })
+        }
+        let existingMail = await prismaDb.user.findFirst({where:{email}});
+        if(existingMail){
+            return res.status(400).json({message:"User exist!"})
+        }
+        const hassPass = await bcrypt.hash(password, 3);
+        if (!hassPass) {
+            return res.status(500).json({message: "Service error!" })
+        }
+        
+        let response = await prismaDb.user.create({
+            data: {
+                name: name, email: email, role: role, password: hassPass
+            }
+        })
+        console.log(response)
+        if (!response) {
+            return res.status(400).json({message: "Db failed!" })
+        }
+        return res.status(201).json({ data:response ,message: "Register success!"})
+    } catch (e) {
+        return res.status(500).json({ success: false, message: "Server error!" })
+    }
+
 })
+
 app.listen(3000)
