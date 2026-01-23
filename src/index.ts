e3646969499c8c058d181be4288757c4aa79a260
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-23 14:23:18 +0530

    chore:files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import express from "express";
-
+import express, { json } from "express";
+import cors from "cors"
 const app = express();
+app.use(cors());
+app.use(json())
 app.get("/hello",(req,res)=>{
     res.send("hello")
 })
