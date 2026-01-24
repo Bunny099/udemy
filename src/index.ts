5f10bcb360e7b5d4df9fa6ba3f48cc7e9349dfc4
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-24 22:56:59 +0530

    update:register & login route update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
 import { prismaDb } from "./lib/db.js";
+import { authMiddleware } from "./middleware/auth.middleware.js";
 
 
 const app = express();
@@ -18,27 +19,30 @@ app.post("/register", async (req, res) => {
         if (!name || !email || !password || !role) {
             return res.status(400).json({ message: "Field missing!" })
         }
-        let existingMail = await prismaDb.user.findFirst({where:{email}});
-        if(existingMail){
-            return res.status(400).json({message:"User exist!"})
+        if (role !== "Instructor" || role !== "Student") {
+            return res.status(400).json({ message: "role can't exist out of scope!" })
+        }
+        let existingMail = await prismaDb.user.findFirst({ where: { email } });
+        if (existingMail) {
+            return res.status(409).json({ message: "User exist!" })
         }
         const hassPass = await bcrypt.hash(password, 3);
         if (!hassPass) {
-            return res.status(500).json({message: "Service error!" })
+            return res.status(500).json({ message: "Service error!" })
         }
-        
+
         let response = await prismaDb.user.create({
             data: {
                 name: name, email: email, role: role, password: hassPass
             }
         })
-        console.log(response)
+       
         if (!response) {
-            return res.status(400).json({message: "Db failed!" })
+            return res.status(400).json({ message: "Db failed!" })
         }
-        return res.status(201).json({ data:response ,message: "Register success!"})
+        return res.status(201).json({ data: response, message: "Register success!" })
     } catch (e) {
-        return res.status(500).json({message: "Server error!" })
+        return res.status(500).json({ message: "Server error!" })
     }
 
 })
@@ -46,21 +50,25 @@ app.post("/login", async (req, res) => {
     try {
         const { email, password, role } = req.body;
         if (!email || !password || !role) {
-            return res.status(400).json({message: "Field missing!" })
+            return res.status(400).json({ message: "Field missing!" })
+        }
+        if (role !== "Instructor" || role !== "Student") {
+            return res.status(400).json({ message: "role can't exist out of scope!" })
         }
 
         let existingUser = await prismaDb.user.findFirst({ where: { email, role } });
         if (!existingUser) {
-            return res.status(404).json({ message: "Not found!" })
+            return res.status(401).json({ message: "Not found!" })
         }
         let isPassValid = await bcrypt.compare(password, existingUser?.password as string);
         if (!isPassValid) {
-            return res.status(400).json({message: "Password invalid!" })
+            return res.status(400).json({ message: "Password invalid!" })
         }
-        let token = jwt.sign({ id: existingUser?.id }, AUTH_SECRET);
-        return res.status(200).json({ data:token, message: "Login success!"})
+        let token = jwt.sign({ id: existingUser?.id, role: existingUser?.role }, AUTH_SECRET);
+        return res.status(200).json({ data: token, message: "Login success!" })
     } catch (e) {
-        return res.status(500).json({message: "Server error!" })
+        return res.status(500).json({ message: "Server error!" })
     }
 })
+
 app.listen(3000)
