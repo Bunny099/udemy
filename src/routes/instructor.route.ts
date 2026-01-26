80b84de468b569d327f617ef8f9a820841d743e2
Bunny099 <jayeshkhuman121@gmail.com>
2026-01-26 19:20:29 +0530

    feat:(instructor) routes for lesson

--- src/routes/instructor.route.ts
+++ src/routes/instructor.route.ts
@@ -1,12 +1,21 @@
 
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
-import { deleteCourse, draftCourse, getCourse ,patchCourse,publishCourse} from "../controllers/instructor.controller.js";
+import { deleteCourse, deleteLesson, draftCourse, getCourse ,getLesson,patchCourse,patchLesson,postLesson,publishCourse} from "../controllers/instructor.controller.js";
 const router = Router();
 
+//course routes
 router.get("/course",authMiddleware,getCourse)
 router.post("/course/draft",authMiddleware,draftCourse)
 router.post("/course/publish",authMiddleware,publishCourse)
 router.patch("/course",authMiddleware,patchCourse)
 router.delete("/course",authMiddleware,deleteCourse)
+
+//lessons
+
+router.get("/:courseId/lesson",authMiddleware,getLesson)
+router.post("/course/lesson",authMiddleware,postLesson);
+router.patch("/lesson",authMiddleware,patchLesson);
+router.delete("/lesson",authMiddleware,deleteLesson)
+
 export default router;
